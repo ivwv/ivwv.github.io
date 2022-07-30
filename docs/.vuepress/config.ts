674d3e054096d7b7e61c5d8dee27b807b4d41194
sy <sy18959889840@gmail.com>
2022-07-30 18:38:25 +0800

    2022-07-30 18-38-25 push

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,8 +7,8 @@ import theme from "./theme";
 
 export default defineUserConfig({
   lang: "zh-CN",
-  title: "主题演示",
-  description: "vuepress-theme-hope 的演示",
+  title: "SY's Blog",
+  description: "SY's Blog",
 
   base: "/",
 
