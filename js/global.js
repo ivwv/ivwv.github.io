92164dd37a37ba150da95002e891b6db473a3509
ivwv <99464244+ivwv@users.noreply.github.com>
2022-07-31 16:15:16 +0000

    Deploying to gh-pages from @ ivwv/ivwv.github.io@df7f9aff4277dce404f466689af334a88a1324e2 🚀

---
+++ js/global.js
@@ -0,0 +1,16 @@
+// 全局js 插入在页面最顶部
+// seo 优化基础网址
+// document.querySelector('head > meta[property="og:url"]').content
+// 'https://ivwv.github.io/'
+// icon 图标
+// document.querySelector('head > link[rel="apple-touch-icon"]').href;
+// 'https://ivwv.github.io/logo.png'
+// console.log("2");
+
+/**
+ * 注意
+ * 1. 目前发现这里的js脚本会被执行两次
+ */
+// setTimeout(function () {
+//   alert("2");
+// }, 4000);
