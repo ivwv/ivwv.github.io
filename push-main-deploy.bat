a025278d83d6203fd1e818b6d859edacdbec9214
sy <sy18959889840@gmail.com>
2022-07-31 11:04:29 +0800

    娴嬭瘯bat

--- push-main-deploy.bat
+++ push-main-deploy.bat
@@ -1,6 +1,7 @@
-@REM 不能输入中文，会乱码
+@REM 不能输入中文，否者会乱码
 git add .
 @REM git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%-%time:~3,2%-%time:~6,2% push"
-git commit -m "update SEO,previre swiper,sitemap,feed and PWD"
+@REM git commit -m "update SEO,previre swiper,sitemap,feed and PWD"
+git commit -m "测试bat"
 git checkout main
 git push -u origin main
