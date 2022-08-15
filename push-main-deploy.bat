383a439382a44c75d9351b2476edb75b0e6f1411
sy <sy18959889840@gmail.com>
2022-08-15 11:50:39 +0800

    bilibili视频下载文章

--- push-main-deploy.bat
+++ push-main-deploy.bat
@@ -1,7 +1,7 @@
-@REM 不能输入中文，否者会乱码
+chcp 65001
 git add .
-git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%-%time:~3,2%-%time:~6,2% push"
+@REM git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%-%time:~3,2%-%time:~6,2% push"
 @REM git commit -m "update SEO,previre swiper,sitemap,feed and PWD"
-@REM git commit -m "updata "
+git commit -m "bilibili视频下载文章"
 git checkout main
 git push -u origin main
